199511bcd0494ce986669e538f1c28e72a1e7995
BeanTheAlien <ben.allen.goldstein@gmail.com>
2026-08-21 14:37:38 -0500

    update random to include randoms

--- Java/src/main/java/utils/Random.java
+++ Java/src/main/java/utils/Random.java
@@ -23,6 +23,17 @@ public class Random {
     public static int random(int a, int b) {
         return (int)Random.ranFloat(a, b);
     }
+    public static Array<Integer> randoms(int count) {
+        return Random.randoms(101, count);
+    }
+    public static Array<Integer> randoms(int max, int count) {
+        return Random.randoms(0, max, count);
+    }
+    public static Array<Integer> randoms(int min, int max, int count) {
+        var arr = new Array<Integer>();
+        for(int i = 0; i < count; i++) arr.add(Random.random(min, max));
+        return arr;
+    }
     public static boolean chance(int floor, int ceil) {
         return Random.random(ceil) <= floor;
     }
